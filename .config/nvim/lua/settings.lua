d0a86a00b5a242e36256b5b843ee7dc715c2bc01
Marat Amirov <marat.amirov@vkpay.com>
2022-06-11 18:07:47 +0300

    move neovim config to settings

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,35 @@
+HOME = os.getenv("HOME")
+
+-- disable auto-loading of plugins
+vim.o.loadplugins = false
+-- auto-update a file
+vim.o.autoread = true
+-- set UTF-8 encoding
+vim.o.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
+vim.o.termencoding = "utf-8"
+-- search relative to current file + directory
+vim.o.path = ".,,**"
+-- tags relative to current file + directory
+vim.o.tags = "./tags;,tags;"
+-- enhance command-line completion
+vim.o.wildmenu = true
+vim.o.wildmode = "list:longest,full"
+-- prevent redraw while executing macros
+vim.o.lazyredraw = true
+-- switch syntax highlighting off
+syntax off
+-- highlight matching braces
+vim.o.showmatch = true
+-- highlight matching pairs of brackets (default "(:),{:},[:]")
+vim.o.matchpairs = vim.o.matchpairs .. ",<:>"
+-- configure tabwidth and insert spaces instead of tabs
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+-- add full file path to statusline
+vim.o.statusline = vim.o.statusline .. "%F %=%l:%c"
+-- centralize backups and swapfiles
+vim.o.undodir = HOME .. "/.vim/tmp/undo//"
+vim.o.backupdir = HOME .. "/.vim/backups//"
+vim.o.directory = HOME .. "/.vim/swaps//"
