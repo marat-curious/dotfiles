38536933edc37937d629753ccf1be48430074045
Marat Amirov <marat.amirov@vkpay.com>
2022-07-20 10:51:19 +0300

    add treesitter package

--- .config/nvim/lua/packages.lua
+++ .config/nvim/lua/packages.lua
@@ -1,5 +1,6 @@
 require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
   use("neovim/nvim-lspconfig")
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
   use({ "catppuccin/nvim", as = "catppuccin" })
 end)
