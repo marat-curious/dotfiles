47704b7ffa336b8ad7b11772e2e958113eebf901
Marat Amirov <m.amirov@vk.team>
2022-12-03 23:48:05 +0300

    try highlight whitespaces

--- .emacs.d/lisp/init-settings.el
+++ .emacs.d/lisp/init-settings.el
@@ -6,4 +6,18 @@
 	inhibit-startup-message t
 	initial-scratch-message nil)
 
+(global-whitespace-mode 1)
+(setq
+	whitespace-style '(
+		face
+		trailing
+		lines-tail
+		space-before-tab
+		space-after-tab
+		newline
+		indentation
+		empty
+	)
+	whitespace-line-column 80)
+
 (provide 'init-settings)
