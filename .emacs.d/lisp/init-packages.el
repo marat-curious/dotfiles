465602b4cddcd74867364fc16f70cd229130f29e
Marat Amirov <m.amirov@vk.team>
2022-12-04 15:54:22 +0300

    update package archives

--- .emacs.d/lisp/init-packages.el
+++ .emacs.d/lisp/init-packages.el
@@ -3,9 +3,16 @@
 (require 'package)
 
 ;; Standard package repositories
-(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '(
+	;; ("gnu" . "https://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")
+	("melpa-stable" . "https://stable.melpa.org/packages/")) t)
 
 (setq package-enable-at-startup nil)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
 (package-initialize)
 
 (provide 'init-packages)
