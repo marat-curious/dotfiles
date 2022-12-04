27f16ef99e6b5f0003f6421d75582c3ab2244e00
Marat Amirov <m.amirov@vk.team>
2022-12-04 15:54:54 +0300

    update go-mode

--- .emacs.d/lisp/init-golang.el
+++ .emacs.d/lisp/init-golang.el
@@ -1,7 +1,12 @@
 ;; Support for Go language -*- lexical-binding: t -*-
 
-(require 'project)
+(unless (package-installed-p 'go-mode)
+	(package-install 'go-mode))
+
+(add-auto-mode 'go-mode '(cons "\\.go\\'" 'go-mode))
+
 (require 'go-mode)
+(require 'project)
 
 (defun project-find-go-module (dir)
   (when-let ((root (locate-dominating-file dir "go.mod")))
@@ -10,7 +15,9 @@
 (cl-defmethod project-root ((project (head go-module)))
   (cdr project))
 
-(add-hook 'go-mode-hook 'eglot-ensure)
+(with-eval-after-load 'eglot
+	(add-hook 'go-mode-hook 'eglot-ensure))
+
 (add-hook 'project-find-functions #'project-find-go-module)
 
 (provide 'init-golang)
