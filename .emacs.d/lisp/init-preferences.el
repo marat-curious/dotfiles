834eb800e980c7204b6aa9d54d32e96d40455d3c
Marat Amirov <m.amirov@vk.team>
2023-01-17 20:00:03 +0300

    update preferences

--- .emacs.d/lisp/init-preferences.el
+++ .emacs.d/lisp/init-preferences.el
@@ -2,13 +2,22 @@
 
 (column-number-mode 1)
 
+(set-default-coding-systems 'utf-8)
+
 (global-set-key [?\C-h] 'delete-backward-char)
 (global-set-key [?\C-x ?h] 'help-command)
 
-(setq-default indent-tabs-mode nil)
-(setq tab-width 4)
+(setq-default
+    fill-column 80
+    indent-tabs-mode nil
+    tab-always-indent 'complete
+    tab-width 4
+    use-short-answers t)
 
 (setq
+    auto-save-default nil
+    create-lockfiles nil
+    make-backup-files nil
     inhibit-startup-message t
     initial-scratch-message nil
     ring-bell-function 'ignore
