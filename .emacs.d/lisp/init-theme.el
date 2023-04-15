f336249cf68e51ddc932c495f6d8e12d09ceee4b
Marat Amirov <m.amirov@vk.team>
2023-04-16 00:04:12 +0300

    change to eink theme

--- .emacs.d/lisp/init-theme.el
+++ .emacs.d/lisp/init-theme.el
@@ -1,10 +1,5 @@
 ;; Theme -*- lexical-binding: t -*-
 
-(load-theme 'tsdh-dark t)
-
-(defun load-tsdh-dark-theme (_frame)
-  (enable-theme 'tsdh-dark))
-
-(add-hook 'after-make-frame-function #'load-tsdh-dark-theme)
+(load-theme 'eink t)
 
 (provide 'init-theme)
