c02204728c1e8b0147d326b955a8ece789c1f744
Marat Amirov <m.amirov@vk.team>
2023-07-15 17:47:20 +0300

    update ido config

--- .emacs.d/lisp/init-ido.el
+++ .emacs.d/lisp/init-ido.el
@@ -7,6 +7,13 @@
 (ido-everywhere 1)
 (ido-ubiquitous-mode 1)
 
-(setq ido-enable-flex-matching t)
+(add-to-list 'ido-ignore-directories "node_modules")
+
+(setq
+ ido-enable-flex-matching t
+ ido-use-filename-at-point 'guess)
+
+(require 'icomplete)
+(icomplete-mode 1)
 
 (provide 'init-ido)
